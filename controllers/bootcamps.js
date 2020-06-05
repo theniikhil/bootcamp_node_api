67891d624fd3014987dcdea3ac8bc7ba7b77348f
theniikhil <imn.parashar@gmail.com>
2020-06-05 11:21:05 +0530

    added pagination, course model,

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   let reqQuery = { ...req.query };
 
-  let removeParams = ['select'];
+  let removeParams = ['select', 'sort', 'page', 'limit'];
 
   removeParams.forEach((param) => delete reqQuery[param]);
 
@@ -25,16 +25,53 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  // select fields
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
   }
 
+  // sort by given fields
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   console.log(queryStr);
   const bootcamps = await query;
+
+  // pagination object
+  const pagination = {};
+
+  if (total > endIndex) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load model
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 //Connect Database
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,10 +23,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // Import Data
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data Created'.green.inverse);
     process.exit();
   } catch (error) {
@@ -37,6 +43,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data Deleted'.red.inverse);
     process.exit();
   } catch (error) {
